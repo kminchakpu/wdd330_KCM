d24c4a7ac732ebe301b72b1ae9d11450a095c943
Kevin Cross Minchakpu <kminchakpu@byupathway.edu>
2026-07-13 18:13:21 +0100

    workied on the individual  activity

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,9 @@
 import { loadHeaderFooter } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
-import ProductList from "./ProductList.mjs";
 import updateCartCount from "./cartCount.mjs";
 import Alert from "./Alert.js";
 
-const dataSource = new ProductData("tents");
-const listElement = document.querySelector(".product-list");
-const productList = new ProductList("tents", dataSource, listElement);
-productList.init();
+loadHeaderFooter();
 updateCartCount();
 
 const alert = new Alert();
 alert.renderAlerts();
-loadHeaderFooter();
---
+++ src/js/product-listing.js
@@ -0,0 +1,13 @@
+import { loadHeaderFooter } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductList from "./ProductList.mjs";
+import updateCartCount from "./cartCount.mjs";
+
+loadHeaderFooter();
+
+const dataSource = new ProductData("tents");
+const listElement = document.querySelector(".product-list");
+const productList = new ProductList("tents", dataSource, listElement);
+
+productList.init();
+updateCartCount();
