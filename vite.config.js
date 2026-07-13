dcc6792a02a65ae1b8166295a7b3d32585a244dd
Kevin Cross Minchakpu <kminchakpu@byupathway.edu>
2026-07-13 10:02:42 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
   root: "src/",
 
   build: {
-    outDir: "../dist",
+    outDir: resolve(__dirname, "dist"),
+    emptyOutDir: true, 
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
