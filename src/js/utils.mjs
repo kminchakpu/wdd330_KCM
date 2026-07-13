8ebdb161fb010c04d013357852aea19cf396be42
Kevin Cross Minchakpu <kminchakpu@byupathway.edu>
2026-07-13 10:56:01 +0100

    update utils.js

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -57,30 +57,25 @@ export function renderListWithTemplate(
   parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
 
-export function renderWithTemplate(
-    template,
-    parentElement,
-    data,
-    callback
-) {
-    parentElement.innerHTML = template;
-
-    if (callback) {
-        callback(data);
-    }
+export function renderWithTemplate(template, parentElement, data, callback) {
+  parentElement.innerHTML = template;
+  if (callback) {
+    callback(data);
+  }
 }
 
 export async function loadTemplate(path) {
-    const response = await fetch(path);
-    const template = await response.text();
-    return template;
+  const response = await fetch(path);
+  return await response.text();
 }
 
 export async function loadHeaderFooter() {
-    const headerTemplate = await loadTemplate("../partials/header.html");
-    const footerTemplate = await loadTemplate("../partials/footer.html");
-    const headerElement = document.querySelector("#main-header");
-    const footerElement = document.querySelector("#main-footer");
-    renderWithTemplate(headerTemplate, headerElement);
-    renderWithTemplate(footerTemplate, footerElement);
+  const headerTemplate = await loadTemplate("../partials/header.html");
+  const footerTemplate = await loadTemplate("../partials/footer.html");
+
+  const headerElement = document.querySelector("#main-header");
+  const footerElement = document.querySelector("#main-footer");
+
+  renderWithTemplate(headerTemplate, headerElement);
+  renderWithTemplate(footerTemplate, footerElement);
 }
