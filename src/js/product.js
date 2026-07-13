b8fe44ece35af9636bdfbbd1e88856962a973e2c
Kevin Cross Minchakpu <kminchakpu@byupathway.edu>
2026-07-13 09:41:52 +0100

    update

--- src/js/product.js
+++ src/js/product.js
@@ -3,7 +3,6 @@ import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 import updateCartCount from "./cartCount.mjs";
 
-
 const dataSource = new ProductData("tents");
 const productID = getParam("product");
 
